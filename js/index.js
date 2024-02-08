5f97d87906f71d7cd99e118c359dbbdf3ed6c6bb
Mohamed Saied <mohamedsaidsalah0111@gmail.com>
2024-02-08 14:33:35 +0200

    added the keyboard even listener

--- js/index.js
+++ js/index.js
@@ -1,47 +1,59 @@
+// Detect Button Press
+
 let drumButtons = document.querySelectorAll(".drum");
 
 for (var i = 0; i < drumButtons.length; i++) {
     drumButtons[i].addEventListener("click", function () {
         var buttonInnerText = this.innerText;
-
-        switch (buttonInnerText) {
-            case "w":
-                var audio = new Audio("sounds/tom-1.mp3");
-                audio.play();
-                break;
-
-            case "a":
-                var audio = new Audio("sounds/tom-2.mp3");
-                audio.play();
-                break;
-
-            case "s":
-                var audio = new Audio("sounds/tom-3.mp3");
-                audio.play();
-                break;
-
-            case "d":
-                var audio = new Audio("sounds/tom-4.mp3");
-                audio.play();
-                break;
-
-            case "j":
-                var audio = new Audio("sounds/snare.mp3");
-                audio.play();
-                break;
-
-            case "k":
-                var audio = new Audio("sounds/crash.mp3");
-                audio.play();
-                break;
-
-            case "l":
-                var audio = new Audio("sounds/kick-bass.mp3");
-                audio.play();
-                break;
-
-            default:
-                console.log(buttonInnerText);
-        }
+        makeSound(buttonInnerText);
     });
+}
+
+// Detect Keyboard Press
+
+document.addEventListener("keypress", function (event) {
+    makeSound(event.key);
+});
+
+// Functions
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+
+        case "d":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+
+        case "j":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+
+        case "k":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+
+        case "l":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+
+        default:
+            console.log(buttonInnerText);
+    }
 }
